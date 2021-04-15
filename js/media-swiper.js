e13f7dc2b89edb67f896668513ca5a09ee540308
tadmouni <58752802+tadmouni@users.noreply.github.com>
2021-04-15 13:17:36 -0500

    Added files

---
+++ js/media-swiper.js
@@ -0,0 +1,84 @@
+var slider1 = new Swiper('.swiper-17', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination-17',
+      clickable: true,
+    },
+    
+    breakpoints: {
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      }
+    },
+    mousewheel: true,
+    keyboard: true,
+    observer: true,
+    observeParents: true,
+});
+
+var slider2 = new Swiper('.swiper-18', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination-18',
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    }
+  },
+  mousewheel: true,
+  keyboard: true,
+  observer: true,
+  observeParents: true,
+
+});
+
+var slider3 = new Swiper('.swiper-19', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination-19',
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    }
+  },
+  mousewheel: true,
+  keyboard: true,
+  observer: true,
+  observeParents: true,
+});
+
+var slider4 = new Swiper('.swiper-20', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination-20',
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    }
+  },
+  mousewheel: true,
+  keyboard: true,
+  observer: true,
+  observeParents: true,
+});
---
+++ js/naples.js
@@ -0,0 +1,30 @@
+
+
+// const config = {
+//   type: 'carousel',
+//   perView: 3,
+//   breakpoints: {
+
+//     800: {
+//       perView: 2
+//     }
+//   }
+// }
+
+// new Glide('.glide', config).mount()
+
+// var input = document.querySelector('#options-start-at-input')
+
+// var glide = new Glide('#options-start-at', {
+//   startAt: input.value,
+//   perView: 3
+// })
+
+// input.addEventListener('input', function (event) {
+//   glide.update({
+//     startAt: event.target.value
+//   })
+// })
+
+// glide.mount()
+
